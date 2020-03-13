5fb0a4de4b58f3346cf57a6de96f033e6b0a024a
颜聿辰 <740987012@qq.com>
2020-03-13 15:08:28 +0800

    版本自检

---
+++ package_version.py
@@ -0,0 +1,12 @@
+import torch
+import tensorflow
+import numpy
+import matplotlib
+
+if __name__ == '__main__':
+    print("torch:", torch.__version__)
+    print("tensorflow:", tensorflow.__version__)
+    print("numpy:", numpy.__version__)
+    print("matplotlib:", matplotlib.__version__)
+
+
